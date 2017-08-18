7e29142791150100e2c7f9831207f0f6806aa4f5
Andriy Bazyuta <andriy.bazyuta@gmail.com>
2017-08-18 18:29:53 +0200

    add rspec model

--- spec/models/route_spec.rb
+++ spec/models/route_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Route, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it { should validate_presence_of(:passphrase_digest) }
+  it { should validate_presence_of(:start_node) }
+  it { should validate_presence_of(:end_node) }
+  it { should validate_presence_of(:source) }
+  
 end
+
