3c570f91606fc66b3542004426b3e9781f254ab7
Andriy Bazyuta <andriy.bazyuta@gmail.com>
2017-08-18 18:43:08 +0200

    Add RSpec controller

---
+++ spec/controllers/routes_controller_spec.rb
@@ -0,0 +1,57 @@
+require 'rails_helper'
+
+
+RSpec.describe RoutesController, type: :controller do
+  
+  
+  describe "GET /routes/" do
+    it "should return list with routes" do
+      route = FactoryGirl.create(:route)
+      
+      get :index, as: :json
+      
+      expect(assigns(:routes)).to eq([route])
+    end
+  end
+
+  describe "POST /routes/" do
+    context "with valid data" do
+      it "should return route and status Created" do
+        
+        params = { 
+          passphrase: Faker::Beer.name, 
+          source: ["sentinels", "sniffers", "loopholes"].sample, 
+          start_node: ["alpha", "beta", "gamma", "delta", "theta", "lambda", "tau", "psi", "omega"].sample, 
+          end_node: ["alpha", "beta", "gamma", "delta", "theta", "lambda", "tau", "psi", "omega"].sample,
+          start_time: "2010-09-06T12:27:00",
+          end_time: "2010-10-06T11:22:02"
+        
+         }
+         
+        get :create, method: :post, params: params, as: :json
+        expect(response).to have_http_status(:created)
+        
+      end
+    end
+
+    context "with invalid data" do
+      it "should return 422 and fail to save" do
+    
+        params = { 
+          passphrase: Faker::Beer.name, 
+          start_node: ["alpha", "beta", "gamma", "delta", "theta", "lambda", "tau", "psi", "omega"].sample, 
+          end_node: ["alpha", "beta", "gamma", "delta", "theta", "lambda", "tau", "psi", "omega"].sample,
+          start_time: "2010-10-06T11:22:02",
+         }
+    
+        get :create, method: :post, params: params, as: :json
+    
+        expect(response).to have_http_status(422)
+        expect(JSON.parse(response.body)).to eq({"error"=>"Validation failed: Source can't be blank, End time Invalid DateTime"})
+        expect(Route.count).to eq(0)
+    
+      end
+    end
+  end
+
+end
--- spec/factories/routes.rb
+++ spec/factories/routes.rb
@@ -2,8 +2,8 @@ FactoryGirl.define do
   factory :route do
     passphrase { Faker::Zelda.character }
     source ["sentinels", "sniffers", "loopholes"].sample
-    start_node ["alpha", "beta", "gamma", "delta", "theta", "lambda", "tau", "psi", "omega"]
-    end_node ["alpha", "beta", "gamma", "delta", "theta", "lambda", "tau", "psi", "omega"]
+    start_node ["alpha", "beta", "gamma", "delta", "theta", "lambda", "tau", "psi", "omega"].sample
+    end_node ["alpha", "beta", "gamma", "delta", "theta", "lambda", "tau", "psi", "omega"].sample
     start_time "2010-09-06T12:27:00"
     end_time "2010-10-06T11:22:02"
   end
